a0f2e70f84c1cad9d737c43f030e95bca4a31023
Chenyang Liu <chenyl@microsoft.com>
2018-08-21 17:28:27 +0800

    Enable cros

--- tests/connection.test.ts
+++ tests/connection.test.ts
@@ -1,6 +1,24 @@
 import * as signalR from "@aspnet/signalr";
+import {LogLevel} from "@aspnet/signalr";
 
-test('connect to server', () => {
+test('connect to server', async () => {
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl('http://localhost:5001/chat', {
+      logger: LogLevel.Trace
+    })
+    .build();
+  const testMsg = "Test Message";
+
+  connection.on("send", data => {
+    expect(data).toBe(testMsg);
+  });
+
+  await connection.start()
+    .catch(err => expect(err).toBeNull());
+
+});
+
+test('test echo', async () => {
   const connection = new signalR.HubConnectionBuilder()
     .withUrl('/chat')
     .build();
@@ -10,6 +28,7 @@ test('connect to server', () => {
     expect(data).toBe(testMsg);
   });
 
-  connection.start()
-    .then(() => connection.invoke("send", testMsg));
+  await connection.start()
+    .then(() => connection.invoke('send', 'connection1', testMsg))
+    .catch(err => expect(err).toBeNull());
 });
