7ddb114bb83c5857fd799a909c92a0d3215d3363
Chenyang Liu <chenyl@microsoft.com>
2018-08-30 15:04:03 +0800

    Add missed hub

---
+++ server/Hub/GenericChat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Test.Server
+{
+    public class GenericChat<T> : Hub<T> where T : class, IChat
+    {
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).Echo(name, message);
+        }
+    }
+
+    public interface IChat
+    {
+        Task Echo(String name, String message);
+    }
+}
---
+++ server/Hub/LongNameChat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.SignalR.Test.Server
+{
+    public class LongNameChatLongNameChatLongNameChatLongNameChatLongNameChatLongNameChatLongNameChatLongNameChatLongNameChatLongNameChatLongName : Chat
+    {
+    }
+}
