09d39811a789ce8f1cf1248a50e544eee9f61e69
Kevin Zhao <kvn.zhl@gmail.com>
2018-08-30 16:25:52 +0800

    Add test.sh to test a single version of Service SDK (#1)

--- build-image.sh
+++ build-image.sh
@@ -5,6 +5,9 @@ set -x
 DIR=$(cd `dirname $0`; pwd)
 pushd ${DIR}
 
+# Build the base docker image
+docker build -t signalr-test-base -f ./RuntimeBase.Dockerfile .
+
 declare -a SDKVersion=("1.0.0-preview1-10009" "1.0.0-preview1-10011" "1.0.0-preview1-10015")
 
 for version in "${SDKVersion[@]}"; do
---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(cd `dirname $0`; pwd)
+VERSION=$1
+CONNECTION_STRING=$2
+
+if [ -z "$VERSION" ]
+then
+    echo SDK version not found!
+    exit 1
+fi
+
+if [ -z "$CONNECTION_STRING" ]
+then
+    echo Azure SignalR Service connection string not found!
+    exit 1
+fi
+
+pushd ${DIR}
+
+docker build -q -t signalr-test-base -f ./RuntimeBase.Dockerfile .
+docker build -q -t signalr-test:$VERSION --build-arg SDKVersion=$VERSION .
+docker run --rm -e Azure__SignalR__ConnectionString="${CONNECTION_STRING}" signalr-test:${VERSION}
+
+popd
