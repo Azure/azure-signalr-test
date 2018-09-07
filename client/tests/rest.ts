ee54c63e4c2abce463df078f7a9b536e9e5b6aeb
Zhenghui Yan <vwxyzh@gmail.com>
2018-09-07 13:21:00 +0800

    extract method.

--- client/tests/rest.ts
+++ client/tests/rest.ts
@@ -1,36 +1,29 @@
 import {ConnectionString} from "./connectionString";
 import * as request from "request-promise";
 
+async function sendRequest(url:string, method:string, target:string, args: string[]){
+  await request({
+    method: method,
+    uri: url,
+    headers: {
+      'Authorization': 'Bearer ' + ConnectionString.getToken(url)
+    },
+    body: {
+      target: target,
+      arguments: args
+    },
+    json: true
+  });
+}
+
 export class Rest {
   public static readonly broadcast = async function (hub:string, target:string, args:string[]) {
     let url = ConnectionString.getPreviewRestUrl(hub);
-    await request({
-      method: 'POST',
-      uri: url,
-      headers: {
-        'Authorization': 'Bearer ' + ConnectionString.getToken(url)
-      },
-      body: {
-        target: target,
-        arguments: args
-      },
-      json: true
-    });
+    await sendRequest(url, 'POST', target, args);
   };
 
   public static readonly sendToUser = async function (hub:string, userId:string, target:string, args:string[]) {
     let url = ConnectionString.getPreviewRestUrl(hub) + '/user/' + userId;
-    await request({
-      method: 'POST',
-      uri: url,
-      headers: {
-        'Authorization': 'Bearer ' + ConnectionString.getToken(url)
-      },
-      body: {
-        target: target,
-        arguments: args
-      },
-      json: true
-    });
+    await sendRequest(url, 'POST', target, args);
   }
 };
