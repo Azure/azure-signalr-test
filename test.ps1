8111206960a2cb089593f8b9b734382747fd2b98
ArchangelSDY <Archangel.SDY@gmail.com>
2018-11-06 11:16:01 +0800

    Add powershell test script (#13)

---
+++ test.ps1
@@ -0,0 +1,13 @@
+param (
+    [Parameter(Mandatory=$true)][string]$version,
+    [Parameter(Mandatory=$true)][string]$connectionString,
+    [bool]$quiet = $true
+)
+
+if ($quiet) {
+    $dockerBuildArgs = "-q"
+}
+
+docker build $dockerBuildArgs -t signalr-test-base -f ./RuntimeBase.Dockerfile .
+docker build $dockerBuildArgs -t signalr-test:$version --build-arg SDKVersion=$version .
+docker run --rm -e Azure__SignalR__ConnectionString="$connectionString" -e DELAY=1000 signalr-test:$version
