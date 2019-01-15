30e0b485685d56fdab34a13bc836fbdceb8f06cd
Raphael Yu <xianyu@microsoft.com>
2019-01-15 14:07:48 +0800

    Update the sdk version.

--- build-image.sh
+++ build-image.sh
@@ -8,10 +8,11 @@ pushd ${DIR}
 # Build the base docker image
 docker build -t signalr-test-base -f ./RuntimeBase.Dockerfile .
 
-declare -a SDKVersion=("1.0.0-preview1-10009" "1.0.0-preview1-10011" "1.0.0-preview1-10015" "1.0.0-preview1-10200" "1.0.0" "1.0.1")
+declare -a SDKVersion=("1.0.0" "1.0.1" "1.0.2" "1.0.3" "1.0.4" "1.0.5")
 
 for version in "${SDKVersion[@]}"; do
     docker build -t signalr-test:$version --build-arg SDKVersion=$version .
+    docker tag signalr-test:$version genevadev.azurecr.io/signalr-test:$version
 done
 
 popd
