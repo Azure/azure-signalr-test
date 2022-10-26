045c5ea0bc7427d37c97541388a5386a158243d2
Terence Fan <stdrickforce@gmail.com>
2022-10-26 14:55:36 +0800

    update (#35)
    Co-authored-by: Terence Fan <tefa@microsoft.com>

--- SDKTest/tests/constant.ts
+++ SDKTest/tests/constant.ts
@@ -1,20 +1,20 @@
 export class Constant {
-  public static readonly broadcast = 'broadcast';
-  public static readonly echo = 'echo';
-  public static readonly sendOthers = 'sendothers';
-  public static readonly joinGroup = 'joingroup';
-  public static readonly leaveGroup = 'leavegroup';
-  public static readonly sendGroup = 'sendgroup';
-  public static readonly sendGroups = 'sendgroups';
-  public static readonly sendGroupExcept = 'sendgroupexcept';
-  public static readonly sendOthersInGroup = 'sendothersingroup';
-  public static readonly sendUser = 'senduser';
-  public static readonly sendUsers = 'sendusers';
-  public static readonly getConnectionId = 'getconnectionid';
+  public static readonly broadcast = "broadcast";
+  public static readonly echo = "echo";
+  public static readonly sendOthers = "sendothers";
+  public static readonly joinGroup = "joingroup";
+  public static readonly leaveGroup = "leavegroup";
+  public static readonly sendGroup = "sendgroup";
+  public static readonly sendGroups = "sendgroups";
+  public static readonly sendGroupExcept = "sendgroupexcept";
+  public static readonly sendOthersInGroup = "sendothersingroup";
+  public static readonly sendUser = "senduser";
+  public static readonly sendUsers = "sendusers";
+  public static readonly getConnectionId = "getconnectionid";
 
   public static readonly port = parseInt(process.env.PORT) || 80;
 
-  public static readonly Server = function () {
+  public static readonly Server = (function () {
     let port = Constant.port;
     let host = `http://localhost:${port}`;
     return {
@@ -23,10 +23,12 @@ export class Constant {
       ChatUrl: `${host}/chat`,
       ChatJwtUrl: `${host}/chatJwt`,
       JwtLoginUrl: `${host}/Jwt/login`,
-    }
-  }();
+    };
+  })();
 
   public static readonly delay = Number(process.env.DELAY || 1000);
   public static readonly timeout = Number(process.env.TIMEOUT || 60000); // Timeout for each test
-  public static readonly awaitTimeout = Number(process.env.TIMEOUT_AWAIT || 1000); // Timeout for await operation
+  public static readonly awaitTimeout = Number(
+    process.env.TIMEOUT_AWAIT || 1000
+  ); // Timeout for await operation
 }
--- SDKTest/tests/hub.test.ts
+++ SDKTest/tests/hub.test.ts
@@ -1,33 +1,33 @@
 import { getConnections, startConnections, stopConnections } from "./utils";
 import { Constant } from "./constant";
 
-const testMessage = 'Test Message';
+const testMessage = "Test Message";
 
-test('generic hub', async () => {
+test("generic hub", async () => {
   const connections = getConnections(1, `${Constant.Server.Host}/genericchat`);
-  const connectionName = 'connection';
+  const connectionName = "connection";
 
   const echoCallback = jest.fn();
-  connections[0].on('echo', echoCallback);
+  connections[0].on("echo", echoCallback);
 
   await startConnections(connections);
 
-  await connections[0].invoke('echo', connectionName, testMessage);
+  await connections[0].invoke("echo", connectionName, testMessage);
 
   expect(echoCallback).toBeCalledWith(connectionName, testMessage);
   await stopConnections(connections);
 });
 
-test('long name hub with length 128', async () => {
+test("long name hub with length 128", async () => {
   const connections = getConnections(1, `${Constant.Server.Host}/longnamechat`);
-  const connectionName = 'connection';
+  const connectionName = "connection";
 
   const echoCallback = jest.fn();
-  connections[0].on('echo', echoCallback);
+  connections[0].on("echo", echoCallback);
 
   await startConnections(connections);
 
-  await connections[0].invoke('echo', connectionName, testMessage);
+  await connections[0].invoke("echo", connectionName, testMessage);
 
   expect(echoCallback).toBeCalledWith(connectionName, testMessage);
   await stopConnections(connections);
