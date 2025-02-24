9b355d4d61a91db1d0f940d7e00751473b438854
Terence Fan <stdrickforce@gmail.com>
2025-02-24 17:41:21 +0800

    update .net 7.0 to 8.0

--- .sh/run-simple-echo.sh
+++ .sh/run-simple-echo.sh
@@ -4,6 +4,6 @@ dotnet restore
 dotnet user-secrets set Azure:SignalR:ConnectionString "$defaultconnstring"
 
 # Check the result
-echo "test net 7.0"
-dotnet run -f  net7.0 | tee "log"
+echo "test net 8.0"
+dotnet run -f  net 8.0 | tee "log"
 grep "Hello!" log
