d56ca0d9047ab9d7e2efa32407148b71e7e00d02
stdrickforce <stdrickforce@gmail.com>
2025-02-25 00:00:00 +0800

    update to net 9.0

--- .sh/run-simple-echo.sh
+++ .sh/run-simple-echo.sh
@@ -4,6 +4,6 @@ dotnet restore
 dotnet user-secrets set Azure:SignalR:ConnectionString "$defaultconnstring"
 
 # Check the result
-echo "Run SimpleEcho test with net 8.0"
-dotnet run -f  net 8.0 | tee "log"
+echo "Run SimpleEcho test with net 9.0"
+dotnet run -f net 9.0 | tee "log"
 grep "Hello!" log
