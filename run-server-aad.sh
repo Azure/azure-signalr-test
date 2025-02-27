0da50cc060cdef103af887bbb2e3498ad8835a77
stdrickforce <stdrickforce@gmail.com>
2025-02-27 18:04:11 +0800

    remove codes

--- run-server-aad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-SERVER_AAD=1 dotnet server/Server.dll > /dev/null &
-
-# Wait for server to have started
-echo "Wait server for 10s"
-sleep 10
-
-npm test --prefix SDKTestWithAAD
