ea0544d5735ef6ac521eeb42f5493e6acc7dd61a
stdrickforce <stdrickforce@gmail.com>
2025-02-27 18:05:27 +0800

    copy scripts

--- .sh/run-docker-e2e.sh
+++
@@ -1,3 +0,0 @@
-docker run -e Azure__SignalR__ConnectionString="$defaultconnstring" --name signalr-e2e-test genevadev.azurecr.io/signalr-e2e-test && EXIT_STATUS=$?
-docker rm signalr-e2e-test
-exit $EXIT_STATUS
--- .sh/run-simple-echo.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-dotnet restore
-dotnet user-secrets set Azure:SignalR:ConnectionString "$defaultconnstring"
-
-# Check the result
-echo "Run SimpleEcho test with net 9.0"
-dotnet run -f net 9.0 | tee "log"
-grep "Hello!" log
