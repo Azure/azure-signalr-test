4a449ba41b9dec45a1b53ec3cc1cbcefbc624a02
stdrickforce <stdrickforce@gmail.com>
2025-02-27 18:02:41 +0800

    remove unused codes

--- ManagementE2ETest/TestConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManagementE2ETest
-{
-    internal class TestConstants
-    {
-        public const string Message = "Hello client, have a nice day!";
-
-        public const string MethodName = "SendMessage";
-    }
-}
