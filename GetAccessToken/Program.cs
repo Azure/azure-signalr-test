2368745573a384aaf351c12579f38e1ff53d9404
Terence Fan <stdrickforce@gmail.com>
2025-03-06 17:21:45 +0800

    GetAccessToken proj

--- GetAccessToken/Program.cs
+++ GetAccessToken/Program.cs
@@ -8,22 +8,33 @@ internal class Program
     private static async Task Main(string[] args)
     {
         var credential = new DefaultAzureCredential();
-        var request = new TokenRequestContext(["https://signalr.azure.com/.default"]);
-        var token = await credential.GetTokenAsync(request, CancellationToken.None);
+
+        var request = new TokenRequestContext(["https://azure.signalr.com/.default"]);
+
+        var token = await credential.GetTokenAsync(request);
 
         var handler = new JwtSecurityTokenHandler();
 
-        var jwt = handler.ReadJwtToken(token.Token);
+        // Check if the token is valid and can be read
+        if (!handler.CanReadToken(token.Token))
+        {
+            Console.WriteLine("Invalid token.");
+            return;
+        }
 
+        // Read the token
+        var jwtToken = handler.ReadJwtToken(token.Token);
+
+        // Print header information
         Console.WriteLine("Header:");
-        foreach (var header in jwt.Header)
+        foreach (var header in jwtToken.Header)
         {
             Console.WriteLine($"{header.Key}: {header.Value}");
         }
 
         // Print claims (payload)
         Console.WriteLine("\nClaims:");
-        foreach (var claim in jwt.Claims)
+        foreach (var claim in jwtToken.Claims)
         {
             Console.WriteLine($"{claim.Type}: {claim.Value}");
         }
